a9c0eac86df175d93edaf8975d65d2a47665ab06
westenwang <2502755124@qq.com>
2025-04-28 10:10:39 +0800

    feat: update deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -22,7 +22,7 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 // https://astro.build/config
 export default defineConfig({
   site: "https://westenwang.github.io/",
-  base: "/",
+  base: "/westenblog",
   trailingSlash: "always",
   integrations: [
     tailwind(
